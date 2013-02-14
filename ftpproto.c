51062f032fe636b3949836622360cf7f7811cd42
ZRJ <mglrwy@gmail.com>
2013-02-14 18:30:10 +0800

    完成 NLST 命令

--- ftpproto.c
+++ ftpproto.c
@@ -8,7 +8,7 @@
 void ftp_reply(session_t *sess, int status, const char *text);
 void ftp_lreply(session_t *sess, int status, const char *text);
 
-int list_common(session_t *sess);
+int list_common(session_t *sess, int detail);
 int get_port_fd(session_t *sess);
 int get_pasv_fd(session_t *sess);
 int get_transfer_fd(session_t *sess);
@@ -137,7 +137,7 @@ void handle_child(session_t *sess) {
 }
 
 // 列出目录详情
-int list_common(session_t *sess) {
+int list_common(session_t *sess, int detail) {
     DIR *dir = opendir(".");
     if (dir == NULL) {
         return 0;
@@ -153,95 +153,27 @@ int list_common(session_t *sess) {
             continue;
         }
 
-        char perms[] = "----------";
-        perms[0] = '?';
-
-        mode_t mode = sbuf.st_mode;
-        switch(mode & S_IFMT) {
-            case S_IFREG:
-                perms[0] = '-';
-                break;
-            case S_IFDIR:
-                perms[0] = 'd';
-                break;
-            case S_IFLNK:
-                perms[0] = 'l';
-                break;
-            case S_IFIFO:
-                perms[0] = 'p';
-                break;
-            case S_IFSOCK:
-                perms[0] = 's';
-                break;
-            case S_IFCHR:
-                perms[0] = 'c';
-                break;
-            case S_IFBLK:
-                perms[0] = 'b';
-                break;
-        }
-
-        if (mode & S_IRUSR) {
-            perms[1] = 'r';
-        }
-        if (mode & S_IWUSR) {
-            perms[2] = 'w';
-        }
-        if (mode & S_IXUSR) {
-            perms[3] = 'x';
-        }
-        if (mode & S_IRGRP) {
-            perms[4] = 'r';
-        }
-        if (mode & S_IWGRP) {
-            perms[5] = 'w';
-        }
-        if (mode & S_IXGRP) {
-            perms[6] = 'x';
-        }
-        if (mode & S_IROTH) {
-            perms[7] = 'r';
-        }
-        if (mode & S_IWOTH) {
-            perms[8] = 'w';
-        }
-        if (mode & S_IXOTH) {
-            perms[9] = 'x';
-        }
-        if (mode & S_ISUID) {
-            perms[3] = (perms[3] == 'x') ? 's' : 'S';
-        }
-        if (mode & S_ISGID) {
-            perms[6] = (perms[6] == 'x') ? 's' : 'S';
-        }
-        if (mode & S_ISVTX) {
-            perms[9] = (perms[9] == 'x') ? 't' : 'T';
-        }
-
         char buf[1024] = {0};
-        int off = 0;
-        off += sprintf(buf, "%s ", perms);
-        off += sprintf(buf + off, "%3d %-8d %-8d ", sbuf.st_nlink, sbuf.st_uid, sbuf.st_gid);
-        off += sprintf(buf + off, "%8lu ", (unsigned long)sbuf.st_size);
 
-        const char *p_date_format = "%b %e %H:%M";
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        time_t local_time = tv.tv_sec;
-        if (sbuf.st_mtime > local_time || (local_time - sbuf.st_mtime) > 60*60*24*182) {
-            p_date_format = "%b %e %Y";
-        }
-        char datebuf[1024] = {0};
-        struct tm* p_tm = localtime(&local_time);
-        strftime(datebuf, sizeof(datebuf), p_date_format, p_tm);
-        off += sprintf(buf + off, "%s ", datebuf);
-        if (S_ISLNK(sbuf.st_mode)) {
-            char tmp[1024] = {0};
-            readlink(dt->d_name, tmp, sizeof(tmp));
-            off += sprintf(buf + off, "%s -> %s\r\n", dt->d_name, tmp);
+        if (detail) {
+            const char *perms = statbuf_get_perms(&sbuf);
+            const char *datebuf = statbuf_get_date(&sbuf);
+            int off = 0;
+            off += sprintf(buf, "%s ", perms);
+            off += sprintf(buf + off, "%3d %-8d %-8d ", sbuf.st_nlink, sbuf.st_uid, sbuf.st_gid);
+            off += sprintf(buf + off, "%8lu ", (unsigned long)sbuf.st_size);
+            off += sprintf(buf + off, "%s ", datebuf);
+            if (S_ISLNK(sbuf.st_mode)) {
+                char tmp[1024] = {0};
+                readlink(dt->d_name, tmp, sizeof(tmp));
+                off += sprintf(buf + off, "%s -> %s\r\n", dt->d_name, tmp);
+            } else {
+                off += sprintf(buf + off, "%s\r\n", dt->d_name);
+            }
         } else {
-            off += sprintf(buf + off, "%s\r\n", dt->d_name);
+            sprintf(buf, "%s\r\n", dt->d_name);
         }
+
         writen(sess->data_fd, buf, strlen(buf));
     }
     closedir(dir);
@@ -470,7 +402,7 @@ static void do_list(session_t *sess) {
     // 150
     ftp_reply(sess, FTP_DATACONN, "Here comes the directory listing.");
     // 传输列表
-    list_common(sess);
+    list_common(sess, 1);
     // 关闭连接套接字
     close(sess->data_fd);
     // 226
@@ -478,7 +410,18 @@ static void do_list(session_t *sess) {
 }
 
 static void do_nlst(session_t *sess) {
-
+    // 创建连接套接字 
+    if (get_transfer_fd(sess) == 0) {
+        return;
+    }
+    // 150
+    ftp_reply(sess, FTP_DATACONN, "Here comes the directory listing.");
+    // 传输列表
+    list_common(sess, 0);
+    // 关闭连接套接字
+    close(sess->data_fd);
+    // 226
+    ftp_reply(sess, FTP_TRANSFEROK, "Directory send OK.");
 }
 
 static void do_rest(session_t *sess) {
